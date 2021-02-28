b9533a4d297fc009850bccd95cdaf90d9ebb8972
SUJIT KUMAR <67626527+SujitDeveloper@users.noreply.github.com>
2021-03-01 00:12:37 +0530

    Welcome to my Portfolio

---
+++ index.php
@@ -0,0 +1,157 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  
+</head>
+<body>
+
+	<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#"><h2>SujitKumar</h2></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav ml-auto mb-2 mb-g-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+        </li>
+        <!-- <li class="nav-item">
+          <a color='light blue' class="nav-link" href="about.php">About</a>
+        </li> -->
+         <!-- <li class="nav-item">
+          <a class="nav-link" href="#">Achievements</a>
+        </li> -->
+         <li class="nav-item">
+          <a color='light blue'class="nav-link" href="ContactMe.html">Contact</a>
+        </li>
+        <!-- <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Dropdown
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li> -->
+        <li class="nav-item">
+          <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+        </li>
+      </ul>
+      <form class="d-flex">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
+
+<div id="demo" class="carousel slide" data-ride="carousel">
+
+  <!-- Indicators -->
+  <ul class="carousel-indicators">
+    <li data-target="#demo" data-slide-to="0" class="active"></li>
+    <li data-target="#demo" data-slide-to="1"></li>
+    <li data-target="#demo" data-slide-to="2"></li>
+  </ul>
+  
+  <!-- The slideshow -->
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="images/LosAngeles.jpg" alt="Los Angeles" width="1100" height="500">
+    </div>
+    <div class="carousel-item">
+      <img src="images/chicago.jpg" alt="Chicago" width="1100" height="500">
+    </div>
+    <div class="carousel-item">
+      <img src="images/NewYork.jpg" alt="New York" width="1100" height="500">
+    </div>
+  </div>
+  
+  <!-- Left and right controls -->
+  <a class="carousel-control-prev" href="#demo" data-slide="prev">
+    <span class="carousel-control-prev-icon"></span>
+  </a>
+  <a class="carousel-control-next" href="#demo" data-slide="next">
+    <span class="carousel-control-next-icon"></span>
+  </a>
+</div>
+
+
+<section class="my-5">
+	<div class="py-5">
+		<h2 class="text-center">About me</h2>
+	</div>
+
+<div class="container-fluid">
+	<div class="row">
+	   <div class="col-lg-6 col-md-6 col-12">
+		    <img src="images/img1.jpg" class="img-fluid aboutimg"> 
+	   </div>
+	   <div class="col-lg-6 col-md-6 col-12">
+		    <h1 class="display-4">I am Sujit kumar.</h1> 
+		    <h5 class="py-3">
+		    	In high school, I began to tire of the pressure placed on me by my family and their high expectations. <br>
+		    	I gradually stopped my extra-curricular activities, including music and art lessons, and chose to work instead.
+		    </h5>
+		    <a href="about.php" class="btn btn-success"> <p> check more... </p></a>
+	   </div>
+</div>
+</div>
+</section>
+
+<section class="my-5">
+	<div class="py-5">
+		<h2 class="text-center">My Achievement</h2>
+	   </div>
+
+	    <div class="container-fluid">
+	     <div class="row">
+	      <div class="col-lg-4 col-md-4 col-12">
+		     <img src="images/c1.jpg" class="img-fluid pb-3"> 
+	      </div>
+        <div class="col-lg-4 col-md-4 col-12">
+         <img src="images/c2.jpg" class="img-fluid pb-3"> 
+        </div>
+        <div class="col-lg-4 col-md-4 col-12">
+         <img src="images/c3.jpg" class="img-fluid pb-3"> 
+        </div>
+        <div class="col-lg-4 col-md-4 col-12">
+         <img src="images/c4.jpg" class="img-fluid pb-3"> 
+        </div>
+        <div class="col-lg-4 col-md-4 col-12">
+         <img src="images/c5.jpg" class="img-fluid pb-3"> 
+        </div>
+        <div class="col-lg-4 col-md-4 col-12">
+         <img src="images/c6.jpg" class="img-fluid pb-3"> 
+        </div>
+       
+       <!-- <div class="card">
+       <img class="card-img-top" src="img_avatar1.png" alt="Card image">
+      <div class="card-body">
+       <h4 class="card-title">Railway Internship</h4>
+       <p class="card-text"></p>
+      ']' <a href="#" class="btn btn-primary">See Profile</a> -->
+  </div>
+</div>
+
+</div>
+</form>
+<p style="float:right;position:sticky"> Design and Developed by<b><i><u> SUJIT KUMAR</u></i></b></p>
+</body>
+</html>
+
+
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+</html>
